da0c528104450fa343036386b780348b9ad52263
Hamit Mızrak <hamitmizrak@outlook.com>
2024-09-02 13:17:37 +0300

    Mongoose Database

---
+++ models/blog_models.js
@@ -0,0 +1,20 @@
+// MongoDB için veritabanı işlemlerinde kullanmak üzere `BlogModel` adında model oluşturalım
+
+// Mongoose adında ki kütüphaneyi ekle ve bu kütüphaneye erişmek için `mongoose` adını kullan.
+const mongoose = require('mongoose');
+
+// Schema
+const PostSchema = new mongoose.Schema({
+    header: String,
+    content: String,
+    author: String,
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+
+}); //end PostSchema
+
+// Module 
+module.exports = mongoose.model('BlogModel', PostSchema )
+ 
