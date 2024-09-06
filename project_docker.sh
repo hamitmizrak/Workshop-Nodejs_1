6f93ffdb238672dd075839cf9ff3c015db77fe30
Hamit Mızrak <hamitmizrak@outlook.com>
2024-09-06 12:11:19 +0300

    Success

--- project_docker.sh
+++ project_docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# NOT: Eğer Linux üzerinde project_docker.sh çalışmazsa
+# x: eXecute
+chmod +x project_docker.sh
+
+# Docker Compose ayağa kaldır
+# docker-compose up -d
 
 # Docker Compose ile tüm servisleri ayağa kaldır
 docker-compose up --build
--- project_manuel.sh
+++ project_manuel.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Docker Compose ayağa kaldır
-docker-compose up -d
+# NOT: Eğer Linux üzerinde project_manuel.sh çalışmazsa
+# x: eXecute
+chmod +x project_manuel.sh
 
 # Node js ayağa kaldır
 # & = anlamı
 # node index.js komutunun çalıştıktan sonra terminali "bloklayarak" (kilitleyerek) diğer komutlara geçişi engellemesidir. Yani, node index.js çalıştığında terminal bu süreçte bekler ve docker-compose up -d komutunu çalıştırmaz.
-node index.js &
+
+# 1.YOL
+#node index.js &
+
+# 2.YOL
+#npm start
+
+# 3.YOL
+# Eğer biz bir script yazarsak run yazmak zorundayız.
+# npm run start
+
+# 4.YOL Nodemon ile ayağa kaldır
+# node monitoring
+npm run nodemon
+
+# 5.YOL Nodemon ile ayağa kaldır
+# q: terminal çıktısındaki çok detayı gösterme
+#npm run nodemon_q
 
